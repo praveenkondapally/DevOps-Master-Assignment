c17c1e7f90d261659b5ecaad50ac3ace69d263e5
praveenkondapally <43089438+praveenkondapally@users.noreply.github.com>
2026-07-08 11:33:44 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.devops;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("****************************************");
+        System.out.println(" Welcome to ABC Technologies");
+        System.out.println(" DevOps CI/CD Assignment");
+        System.out.println(" Student Name : YOUR NAME");
+        System.out.println(" Roll Number  : YOUR ROLL NUMBER");
+        System.out.println("****************************************");
+
+    }
+
+}
